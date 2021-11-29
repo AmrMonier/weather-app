dde039ae7f9352a2e48220dfa97c1827da24144b
Amr Monier <a.monier2107@gmail.com>
2021-11-29 21:25:30 +0200

    registered EJS as the templating engine

--- server/app.js
+++ server/app.js
@@ -3,8 +3,13 @@ const path = require('path')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.get('/', (req, res, next) => {
+    res.render('index')
+})
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server Running on http://localhost:${PORT}`))
